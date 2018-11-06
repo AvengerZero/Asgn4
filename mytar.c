26bfe08678a0a776caf1ac14e614c488c2ecaa5d
XateAorora <GamerXD8@yahoo.com>
2018-11-05 16:49:50 -0800

    Add files via upload

---
+++ mytar.c
@@ -0,0 +1,12 @@
+//header: include
+//static variable declaration
+static int v=0;
+int main(int argc, char[]* argv){
+    if (argc!=3 ||argc!=4){
+        printf("mytar [ctxvS]f tarfile [ path [ ... ] ]");
+        exit(1);
+    }
+    char *a=char[1]; //for checking each letters
+    int 
+    while (
+}
