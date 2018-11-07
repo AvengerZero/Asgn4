2f992cb6b382ea42b332ccb06d923224c557cac7
XateAorora <GamerXD8@yahoo.com>
2018-11-07 13:46:59 -0800

    Update mytar.c

--- mytar.c
+++ mytar.c
@@ -1,4 +1,8 @@
 //header: include
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>	
+#include <stdlib.h>	
 
 //static variable declaration
 static int v=0,S=0;
@@ -10,12 +14,12 @@ int main(int argc, char[]* argv){
         exit(1);
     }
     char *a=char[1]; //for checking each letters
-    int c,t,x,f;
+    int c=0,t=0,x=0,f=0;
     while (;a!='/0';a++){
-        if (strcmp(a,"c")==0){c=1;}
-        if (strcmp(a,"t")==0){t=1;}
-        if (strcmp(a,"x")==0){x=1;}
-        if (strcmp(a,"f")==0){f=1;}
+        if (strcmp(a,'c')==0){c=1;}
+        if (strcmp(a,'t')==0){t=1;}
+        if (strcmp(a,'x')==0){x=1;}
+        if (strcmp(a,'f')==0){f=1;}
     }
     if (f==0||c+t+x!=1){
         printf("mytar [ctxvS]f tarfile [ path [ ... ] ]");
@@ -24,7 +28,17 @@ int main(int argc, char[]* argv){
     tarfile=argv[2];
     if (argc==4){path=argv[3];}
     else {path=NULL;}
-    if (c==1){creation(tarfile,path,v);}
-    if (t==1){listing(tarfile,v);}
-    if (x==1){extraction(tarfile,path);}
+    if (c==1){printf(creation(tarfile,path,v));} //printf as placeholder
+    if (t==1){printf(listing(tarfile,v));}
+    if (x==1){printf(extraction(tarfile,path));}
+    return 0;
+}
+int creation(tarfile,path,v){
+    return 0;
+}
+int listing(tarfile,v){
+    return 0;
+}
+int extraction(tarfile,path){
+    return 0;
 }
