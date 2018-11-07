01c174c6393c867c7cb28d013ed41cc753d92549
XateAorora <GamerXD8@yahoo.com>
2018-11-07 15:23:06 -0800

    Update mytar.c

--- mytar.c
+++ mytar.c
@@ -3,11 +3,19 @@
 #include <string.h>
 #include <errno.h>	
 #include <stdlib.h>	
+#include <stdint.h> 
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define _BSD_SOURCE
 
 //static variable declaration
 static int v=0,S=0;
 static char* tarfile;
 static char* path;
+//NOTE: Everything deals with archive files as input, each archive file having the long header
 int main(int argc, char[]* argv){
     if (argc!=3 ||argc!=4){
         printf("mytar [ctxvS]f tarfile [ path [ ... ] ]");
@@ -34,11 +42,21 @@ int main(int argc, char[]* argv){
     return 0;
 }
 int creation(tarfile,path,v){
+    printf("%s,%s,%d",*tarfile,*path,v);
     return 0;
 }
 int listing(tarfile,v){
+    //uses archive file, therefore, uses ARCHIVE HEADER
+    //uses mode (left padded with 4 ASCII 0000, 4 ASCII numbers, 0(sticky)XXX (user-group.other)) and \0
+    //uses file type for d or not
+    //with file pointer (while loop), open and see if RIGHT directory (if command), if so, go further and repeat
+    //else print name with dirent
+    //if v is on, does the same, but now with lstat and dirent: permission (directory,user/group/other)
+    //st_uid/st_gid (with getpwuid/getgrgid, st_size, st_mtime    
+    printf("%s,%d",*tarfile,v);
     return 0;
 }
 int extraction(tarfile,path){
+    printf("%s,%s",*tarfile,*path);
     return 0;
 }
