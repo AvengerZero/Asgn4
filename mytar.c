db1d34e36d8034df4bed40be79175f7f0aa258a5
XateAorora <GamerXD8@yahoo.com>
2018-11-09 16:30:21 -0800

    Update mytar.c

--- mytar.c
+++ mytar.c
@@ -11,51 +11,109 @@
 
 //#define _BSD_SOURCE
 //static variable declaration
-static int v=0,S=0;
-static char* tarfile;
-static char* path;
+static int v=0,S=0,numpath=0;
+static char* tarfile=0;
+static char* path=0;
 //NOTE: Everything deals with archive files as input, each archive file having the long header
 int main(int argc, char *argv[]){
-    if (argc!=3 ||argc!=4){
+    if (argc<3){
         printf("mytar [ctxvS]f tarfile [ path [ ... ] ]");
         exit(1);
     }
-    char a=argv[1]; //for checking each letters
+    char* a=argv[1]; //for checking each letters
     int c=0,t=0,x=0,f=0;
-    while (;a != '/0';a++){
-        if (strcmp(&a,'c')==0){c=1;}
-        if (strcmp(&a,'t')==0){t=1;}
-        if (strcmp(&a,'x')==0){x=1;}
-        if (strcmp(&a,'f')==0){f=1;}
+    while (;*a!='/0';a++){
+        if (*a=='c'){c=1;}
+        if (*a=='t'){t=1;}
+        if (*a=='x'){x=1;}
+        if (*a=='v'){v=1;}
+        if (*a=='S'){S=1;}
     }
+    if (*(--a)=='f'){f=1;}
     if (f==0||c+t+x!=1){
         printf("mytar [ctxvS]f tarfile [ path [ ... ] ]");
         exit(1);
     }
     tarfile=argv[2];
-    if (argc==4){path=argv[3];}
-    else {path=NULL;}
-    if (c==1){printf(creation(tarfile,path,v));} //printf as placeholder
-    if (t==1){printf(listing(tarfile,v));}
-    if (x==1){printf(extraction(tarfile,path));}
+    if (argc>=4){path=argv[3]; numpath=argc-3;} //more than 1 argument (when to check search 256+?)
+    if (c==1){printf(creation());} //NOTE: PLACEHOLDER
+    if (t==1){printf(listing());}
+    if (x==1){printf(extraction());}
     return 0;
 }
-int creation(tarfile,path,v){
-    printf("%s,%s,%d",tarfile,path,v);
+//note possible errors: fail to read file, fail to execute certain steps, including malloc
+//Lacking in free command
+//NOTE: IF PATH MORE THAN 256, ERROR
+int creation(){
+    int fd=open(tarfile,O_CREAT|O_RDWR|O_TRUNC,770); //create new with perms
+    if (fd<0){ perror("fd-open");return 1;}
+    tapeArchive *tar=createArchiveFromFile(fd); 
+    //remaining arguments taken as path
+    //while loop if argv[x]!=NULL
+    //set int fd pointer accordingly (recursion)
+    //while opendir loop with int fd ^
+    //if type isn't defined, printf error then go on
+    //if type is directory, open everything inside it for tar
+    //depth first, deepest directory
+    //else, if v, printf file.
+    //printArchiveToFile
+    //note: make smaller function for recursion, book
+    printf("%s,%s,%d",*tarfile,path,v);
     return 0;
 }
-int listing(tarfile,v){
-    //uses archive file, therefore, uses ARCHIVE HEADER
-    //uses mode (left padded with 4 ASCII 0000, 4 ASCII numbers: 0(sticky)XXX (user-group.other)) and \0
-    //uses file type for d or not
-    //with file pointer (while loop), open and see if RIGHT directory (if command), if so, go further and repeat
-    //else print name with dirent
-    //if v is on, does the same, but now with lstat and dirent: permission (directory,user/group/other)
-    //st_uid/st_gid (with getpwuid/getgrgid, st_size, st_mtime    
+int listing(){ //NOTE: CORRUPT HEADER=ABORT
+    //INVALID CHECKSUM, MAGIC NUM, VERSION
+    int fd=open(tarfile,O_RDONLY);
+    if (fd<0){ perror("open");return 1;}
+    char buf[512]=0;
+    //read the thing, then skip size_t step towards next header.
+    struct tapeArchive *tar=createnewTA();
+    while (int a=read(fd,&buf,512)){
+        if (a==-1){perror("read");return 1;}
+        //put things inside [512] string into empty TArchive (handle uid?)
+        tar=fillTA(buf);
+        long int b=strtol(tar->size,NULL,8);
+        if (path!=NULL){
+            //compare path to name: use tar
+            if (strlen(path)>256){
+                perror("pathlen");return 1;
+            }
+            char *pfix=tar->prefix;
+            char *nme=tar->name;
+            strcat(pfix,nme); //final string in pfix
+            char* yes=strstr(pfix,path); //find substring in string
+            if (yes){ //do the traversal
+                //print info
+                if (v==1){printv(tar);}
+                else{ printf("%s/n",tar->name);
+            }
+            else { //skip  
+                read(fd,NULL,b); //skip the contents x-steps
+
+            }
+        }
+        else {//if no path,print everything
+        //checks to see if 
+            if (v==1){printv(tar);}
+            else{ printf("%s/n",tar->name);
+            read(fd,NULL,b); //skip the contents x-steps
+        }
+    }
     printf("%s,%d",tarfile,v);
     return 0;
 }
-int extraction(tarfile,path){
+int extraction(){
     printf("%s,%s",tarfile,path);
     return 0;
 }
+int tarVerify(tarfile){
+    
+}
+void printv(TapeArchive *tar){
+    
+    //uses mode (left padded with 4 ASCII 0000, 4 ASCII numbers, 0(sticky)XXX (user-group.other)) and \0
+    //translate uid and gid into namespace
+    //translate time int into time format
+    //in printing, adds spaces (formatting)
+}
+//timing of errors for path, for header
